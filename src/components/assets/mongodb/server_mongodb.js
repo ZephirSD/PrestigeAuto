9f5db1fe30f1f8a9995a14a77a9bf4d4c9728895
ZephirSD <87165268+ZephirSD@users.noreply.github.com>
2022-08-12 17:08:42 +0400

    Configuration du serveur MongoDB et du système d'API avec ExpressJS

---
+++ src/components/assets/mongodb/server_mongodb.js
@@ -0,0 +1,47 @@
+const mongoose = require("mongoose");
+const express = require("express");
+const app = express();
+const Vehicules = require('./models/Vehicules');
+const Categories = require('./models/Categories');
+const cors = require('cors');
+// const router = require('express').Router()
+
+app.use(
+  express.urlencoded({
+    extended: true,
+  }),
+)
+
+app.use(express.json())
+
+app.use(cors());
+
+mongoose.connect('mongodb://localhost:27017/PrestigeAuto')
+.then(()=>{
+  app.listen(5000);
+  console.log('connecter mongo');
+})
+.catch((err) => console.log(err))
+
+app.get('/api/vehicules', (req, res) => {
+  Vehicules.find({}, (err, result) => {
+    if(err){
+      res.json(err);
+    }
+    else{
+      res.json(result);
+    }
+  })
+})
+
+app.get('/api/categories', (req, res) => {
+  Categories.find({}, (err, result) => {
+    if(err){
+      res.json(err);
+    }
+    else{
+      res.json(result);
+    }
+  })
+})
+
