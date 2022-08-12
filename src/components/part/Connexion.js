96f8597a97e715575eadca2d853360bf0f4f0e1d
ZephirSD <87165268+ZephirSD@users.noreply.github.com>
2022-08-12 12:03:29 +0400

    Insertion du balise `<form>`

--- src/components/part/Connexion.js
+++ src/components/part/Connexion.js
@@ -4,13 +4,15 @@ import { Button, Input } from './Formulaire'
 function Connexion() {
   return (
     <>
-        <div className='case_filter'>
-            <div className='case_connexion'>
-                <Input type={'email'} label={'Email / Pseudo'}/>
-                <Input type={'password'} label={'Mot de passe'}/>
-                <Button type={'button'} value={'Connecter'}/>
-            </div>
-        </div>
+        <form style={{padding: '0px'}}>
+          <div className='case_filter'>
+              <div className='case_connexion'>
+                  <Input type={'email'} label={'Email / Pseudo'}/>
+                  <Input type={'password'} label={'Mot de passe'}/>
+                  <Button type={'button'} value={'Connecter'}/>
+              </div>
+          </div>
+        </form>
     </>
   )
 }
