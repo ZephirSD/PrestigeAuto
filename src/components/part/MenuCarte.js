f44c8cad528694ceb8f27cb49958970a0f274228
ZephirSD <87165268+ZephirSD@users.noreply.github.com>
2022-08-12 11:53:11 +0400

    Système de switch pour l'affichage de la case de Connexion

--- src/components/part/MenuCarte.js
+++ src/components/part/MenuCarte.js
@@ -1,22 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
+import Connexion from './Connexion';
 
-function MenuCarte({ boolConnect }) {
+function MenuCarte({ boolConnect, switchBool }) {
+  const [switchConnexion, setSwitchConnexion] = useState(false);
+  function toggleConnexion(){
+    setSwitchConnexion(switchConnexion => !switchConnexion);
+    if(switchConnexion === true){
+      switchBool = false;
+    }
+    else{
+      switchBool = true;
+    }
+  }
   return (
     <>
-      <div className="flex_carte_menu">
-        {boolConnect === false ? (
-          <>
-            <h2 className="lien_menu_carte">Inscription</h2>
-            <h2 className="lien_menu_carte">Connexion</h2>
-          </>
-        ) : (
-          <>
-            <h2 className="lien_menu_carte">Paramètres</h2>
-            <h2 className="lien_menu_carte">Favoris</h2>
-            <h2 className="lien_menu_carte">Reservations</h2>
-          </>
-        )}
-      </div>
+        <div className="flex_carte_menu">
+          {boolConnect === false ? (
+            <>
+              <h2 className="lien_menu_carte">Inscription</h2>
+              <h2 className="lien_menu_carte" onClick={toggleConnexion}>Connexion</h2>
+            </>
+          ) : (
+            <>
+              <h2 className="lien_menu_carte">Paramètres</h2>
+              <h2 className="lien_menu_carte">Favoris</h2>
+              <h2 className="lien_menu_carte">Reservations</h2>
+            </>
+          )}
+        </div>
+        {
+          switchConnexion === true ? (<Connexion/>): <></>
+        }
     </>
   );
 }
