fb4fb6420c3582301f502da9260c7064001ee224
ZephirSD <87165268+ZephirSD@users.noreply.github.com>
2022-08-13 16:02:13 +0400

    Insertion de la voiture n°5 pour un test

--- src/components/assets/bases_donnees/baseVehicules.js
+++ src/components/assets/bases_donnees/baseVehicules.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import BMW8 from '../3D_model/BMW8/Bmw8';
 import MercedesEQE from '../3D_model/Mercedes_EQE/MercedesEQE';
+import Porsche911 from '../3D_model/Porsche_911_Carrera/Porsche911';
 
 export const baseVehicules =  
 [
     {
         id: 2,
         voiture: <BMW8/>
+    },
+    {
+        id: 5,
+        voiture: <Porsche911/>
     },
     {
         id: 12,
