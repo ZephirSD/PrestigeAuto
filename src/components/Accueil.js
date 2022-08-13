894555ebca6f171cdba7f89c481c94efc1aeefd3
ZephirSD <87165268+ZephirSD@users.noreply.github.com>
2022-08-13 08:36:04 +0400

    Insertion de l'API des Véhicules pour les différents composants

--- src/components/Accueil.js
+++ src/components/Accueil.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from './Header';
 import Recherche from './part/Recherche';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -7,17 +7,26 @@ import GridVehicules from './part/GridVehicules';
 
 
 function Accueil() {
+  const [dataVehicules, setDataVehicules] = useState([]);
+  const fetchVehicules = async () => {
+    const api = await fetch("http://localhost:5000/api/vehicules");
+    const reponse = await api.json();
+    setDataVehicules(reponse);
+  };
+  useEffect(()=>{
+    fetchVehicules();
+  },[])
   return (
     <>
       <main className='main-accueil'>
         <Header/>
-        <Recherche/>
+        <Recherche dataVehicules={dataVehicules}/>
         <section className='section-location-image'>
           <div className='flex-location'>
             <button className='location-boutton'>Prenez un(e) taxi / location <FontAwesomeIcon icon={faArrowRightLong} className='fleche-droite'/></button>
           </div>
         </section>
-        <GridVehicules/>
+        <GridVehicules dataVehicules={dataVehicules}/>
       </main>
     </>
   )
