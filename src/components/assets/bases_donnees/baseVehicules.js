32a5ec91b113c3a4e3cb8a06143ff3111ef83ee5
ZephirSD <87165268+ZephirSD@users.noreply.github.com>
2022-08-13 16:25:14 +0400

    Ajout de la Porsche Taycan dans la `baseVehicules`

--- src/components/assets/bases_donnees/baseVehicules.js
+++ src/components/assets/bases_donnees/baseVehicules.js
@@ -2,12 +2,17 @@ import React from 'react';
 import BMW8 from '../3D_model/BMW8/Bmw8';
 import MercedesEQE from '../3D_model/Mercedes_EQE/MercedesEQE';
 import Porsche911 from '../3D_model/Porsche_911_Carrera/Porsche911';
+import PorscheTaycan from '../3D_model/Porsche_Taycan/Porschetaycan';
 
 export const baseVehicules =  
 [
     {
         id: 2,
         voiture: <BMW8/>
+    },
+    {
+        id: 3,
+        voiture: <PorscheTaycan/>
     },
     {
         id: 5,
