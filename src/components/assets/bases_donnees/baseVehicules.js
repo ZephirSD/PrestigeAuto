640f1dfc1afab339fd67f594ba6e6084bf6c8793
ZephirSD <87165268+ZephirSD@users.noreply.github.com>
2022-08-13 15:36:05 +0400

    Création de la `baseVehicules` pour les véhicules 3D

---
+++ src/components/assets/bases_donnees/baseVehicules.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import BMW8 from '../3D_model/BMW8/Bmw8';
+import MercedesEQE from '../3D_model/Mercedes_EQE/MercedesEQE';
+
+export const baseVehicules =  
+[
+    {
+        id: 2,
+        voiture: <BMW8/>
+    },
+    {
+        id: 12,
+        voiture: <MercedesEQE/>
+    }
+]
