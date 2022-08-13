c1cbbbf9444bbecc800effbed3a036699fcc8c8f
ZephirSD <87165268+ZephirSD@users.noreply.github.com>
2022-08-13 15:40:21 +0400

    Insertion de `ThreeJS` & de la `baseVehicules`

--- src/components/part/GridVehicules.js
+++ src/components/part/GridVehicules.js
@@ -1,8 +1,12 @@
-import React, { useEffect, useState } from "react";
+import React, { Suspense, useEffect, useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
+import { baseVehicules } from "../assets/bases_donnees/baseVehicules";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
+import Loader3D from "./Loader3D";
 
-function GridVehicules({dataVehicules}) {
+function GridVehicules({ dataVehicules }) {
   const [dataCategories, setDataCategories] = useState([]);
   const fetchCategories = async () => {
     const api = await fetch("http://localhost:5000/api/categories");
@@ -18,18 +22,44 @@ function GridVehicules({dataVehicules}) {
         {dataCategories.map((categorie, index) => (
           <>
             <h1 className="titre-vehicules">{categorie.nom_categorie}</h1>
-            <Swiper slidesPerView={3} spaceBetween={20} style={{marginBottom: '60px'}}>
-              {dataVehicules.map((voiture, index) =>
+            <Swiper
+              slidesPerView={3}
+              spaceBetween={20}
+              style={{ marginBottom: "60px" }}
+            >
+              {dataVehicules.map((voiture) =>
                 voiture.table_categorie.map((categoVehi) =>
                   categoVehi === categorie.id_categorie ? (
                     <>
                       <SwiperSlide>
-                        <div className="carte-vehicule">
-                          <div className="case-voiture-render"></div>
-                          <div className="nom-voiture">
-                            {voiture.nom_vehicules}
+                        <>
+                          <div className="carte-vehicule">
+                              <div
+                                className="case-voiture-render"
+                              >
+                                <Canvas>
+                                  <ambientLight intensity={1.5} />
+                                  <OrbitControls />
+                                  <Suspense fallback={<Loader3D/>}>
+                                  {
+                                    baseVehicules.map((base) => (
+                                      <>
+                                        {voiture.id_vehicules === base.id ? (
+                                          base.voiture
+                                        ) : (
+                                          <></>
+                                        )}
+                                      </>
+                                    ))
+                                  }
+                                  </Suspense>
+                                </Canvas>
+                              </div>
+                            <div className="nom-voiture">
+                              {voiture.nom_vehicules}
+                            </div>
                           </div>
-                        </div>
+                        </>
                       </SwiperSlide>
                     </>
                   ) : (
