e0f1f4a74e466565ea8fe07d79405fd162d2637f
ZephirSD <87165268+ZephirSD@users.noreply.github.com>
2022-08-14 15:35:13 +0400

    Insertion de `Routes` & `Route` pour les redirections et insertion de l'API Véhicules

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Route, Routes } from 'react-router-dom';
 import Accueil from './components/Accueil';
+import VehiculesPages from './components/VehiculesPages';
 import  './style/style.scss';
 
 function App() {
+  const [dataVehicules, setDataVehicules] = useState([]);
+  const fetchVehicules = async () => {
+    const api = await fetch("http://localhost:5000/api/vehicules");
+    const reponse = await api.json();
+    setDataVehicules(reponse);
+  };
+  useEffect(() => {
+    fetchVehicules();
+  },[])
   return (
     <>
-      <Accueil/>
+      <Routes>
+          <Route path='/' element={<Accueil dataVehicules={dataVehicules}/>}/>
+          <Route path='/vehicules/:id_voiture' element={<VehiculesPages dataVehicules={dataVehicules}/>}/>
+      </Routes>
     </>
   )
 }
