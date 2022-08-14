a0956eecfcaf53d8c9e875027c40032e1e3c8a8b
ZephirSD <87165268+ZephirSD@users.noreply.github.com>
2022-08-14 15:39:55 +0400

    Insertion du `prix_unitaire` dans le model `Vehicules`

--- src/components/assets/mongodb/models/Vehicules.js
+++ src/components/assets/mongodb/models/Vehicules.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const Vehicules = mongoose.model('vehicules', {
     id_vehicules: Number,
     nom_vehilcules: String,
+    prix_unitaire: Number,
     table_categorie: Array
 })
 
