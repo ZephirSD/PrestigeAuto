defbc44050a47f128ca31b33565af906b6bf4039
ZephirSD <87165268+ZephirSD@users.noreply.github.com>
2022-08-14 15:36:09 +0400

    Création du props `dataVehicules` pour le fetchVehicules

--- src/components/Accueil.js
+++ src/components/Accueil.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import Header from './Header';
 import Recherche from './part/Recherche';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -6,16 +6,7 @@ import { faArrowRightLong } from '@fortawesome/free-solid-svg-icons';
 import GridVehicules from './part/GridVehicules';
 
 
-function Accueil() {
-  const [dataVehicules, setDataVehicules] = useState([]);
-  const fetchVehicules = async () => {
-    const api = await fetch("http://localhost:5000/api/vehicules");
-    const reponse = await api.json();
-    setDataVehicules(reponse);
-  };
-  useEffect(()=>{
-    fetchVehicules();
-  },[])
+function Accueil({dataVehicules}) {
   return (
     <>
       <main className='main-accueil'>
