2c6cd8d442cf1ff074e0d0794713cc68fd0faccd
ZephirSD <87165268+ZephirSD@users.noreply.github.com>
2022-08-15 20:48:50 +0400

    Insertion di lien de location dans l'Accueil

--- src/components/Accueil.js
+++ src/components/Accueil.js
@@ -4,6 +4,7 @@ import Recherche from './part/Recherche';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowRightLong } from '@fortawesome/free-solid-svg-icons';
 import GridVehicules from './part/GridVehicules';
+import { Link } from 'react-router-dom';
 
 
 function Accueil({dataVehicules, dataCategories}) {
@@ -14,7 +15,9 @@ function Accueil({dataVehicules, dataCategories}) {
         <Recherche dataVehicules={dataVehicules}/>
         <section className='section-location-image'>
           <div className='flex-location'>
-            <button className='location-boutton'>Prenez un(e) taxi / location <FontAwesomeIcon icon={faArrowRightLong} className='fleche-droite'/></button>
+            <Link to={'/location'} style={{textDecoration: 'none'}}>            
+              <button className='location-boutton'>Prenez un(e) taxi / location <FontAwesomeIcon icon={faArrowRightLong} className='fleche-droite'/></button>
+            </Link>
           </div>
         </section>
         <GridVehicules dataVehicules={dataVehicules} dataCategories={dataCategories}/>
