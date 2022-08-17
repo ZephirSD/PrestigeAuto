1bac336ebdd2656cedfec95b359cdf4d2b0387cf
ZephirSD <87165268+ZephirSD@users.noreply.github.com>
2022-08-17 18:22:33 +0400

    Suppression du spin de MUI

--- src/components/part/Loader3D.js
+++ src/components/part/Loader3D.js
@@ -1,26 +1,29 @@
 import React from 'react';
 import { Html, useProgress } from "@react-three/drei";
-import { CircularProgress } from '@mui/material';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
+// import { CircularProgress } from '@mui/material';
+// import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 function Loader3D() {
   const { progress } = useProgress();
-  const theme = createTheme({
-    palette: {
-      primary: {
-        main: '#BB6F9C',
-        darker: '#BB6F9C',
-      },
-    },
-  });
+  // const theme = createTheme({
+  //   palette: {
+  //     primary: {
+  //       main: '#BB6F9C',
+  //       darker: '#BB6F9C',
+  //     },
+  //   },
+  // });
     
   return (
     <>
         <Html style={{width: '100%', height: '100%'}} position={[-0.7,0,0]}>
             <section className='section_loader'>
-              <ThemeProvider theme={theme}>
+              {
+                `${Math.round(progress)}%`
+              }!
+              {/* <ThemeProvider theme={theme}>
                   <CircularProgress variant={'determinate'} value={progress} color='primary' size={50}/>
-              </ThemeProvider>
+              </ThemeProvider> */}
             </section>
         </Html>
     </>
