5b9f5d4c71469b2a6e353e31c35363c39317e951
ZephirSD <87165268+ZephirSD@users.noreply.github.com>
2022-08-22 18:14:41 +0400

    Ajout de props pour le changement d'état de l'affichage de `Connexion`

--- src/components/part/Connexion.js
+++ src/components/part/Connexion.js
@@ -1,20 +1,22 @@
 import React from 'react'
 import { Button, Input } from './Formulaire'
 
-function Connexion() {
+function Connexion({boolToggle}) {
+  const clickCaseFilter = () => {
+    boolToggle(false);
+  }
   return (
-    <>
-        <form style={{padding: '0px'}}>
-          <div className='case_filter'>
-              <div className='case_connexion'>
-                  <Input type={'email'} label={'Email / Pseudo'}/>
-                  <Input type={'password'} label={'Mot de passe'}/>
-                  <Button type={'button'} value={'Connecter'}/>
-              </div>
-          </div>
-        </form>
-    </>
-  )
+      <>
+          <form style={{padding: '0px'}}>
+            <div className='case_filter' onClick={clickCaseFilter}></div>
+            <div className='case_connexion'>
+                <Input type={'email'} label={'Email / Pseudo'}/>
+                <Input type={'password'} label={'Mot de passe'}/>
+                <Button type={'button'} value={'Connecter'}/>
+            </div>
+          </form>
+      </>
+    )
 }
 
 export default Connexion
