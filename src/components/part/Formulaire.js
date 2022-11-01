231e4a0e5e73b1cf741b2d42060a31c6da833a43
ZephirSD <87165268+ZephirSD@users.noreply.github.com>
2022-11-01 13:42:10 +0400

    Ajout de props pour la personnalisation des composants `Input` & `Button`

--- src/components/part/Formulaire.js
+++ src/components/part/Formulaire.js
@@ -1,21 +1,21 @@
 import React from "react";
 
-function Input({label, type, ref}) {
+function Input({label, type, ref, value, changeValue, style}) {
   return (
     <>
-      <div className="case_input">
+      <div className="case_input" style={style}>
         <label>{label}</label>
-        <input type={type} ref={ref}></input>
+        <input type={type} ref={ref} value={value} onChange={changeValue}></input>
       </div>
     </>
   );
 }
 
-function Button({value, type}) {
+function Button({value, type, onClick}) {
   return (
     <>
-      <div className="case_input">
-        <button type={type}>{value}</button>
+      <div className="case_input">, 
+        <button type={type} onClick={onClick !== "" || onClick != null ?  onClick : ""}>{value}</button>
       </div>
     </>
   )
