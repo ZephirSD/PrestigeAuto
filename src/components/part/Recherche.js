51f2cb742162caf64049105ff698866db16d03aa
ZephirSD <87165268+ZephirSD@users.noreply.github.com>
2022-11-01 13:38:26 +0400

    Ajout de la fonctionnalité permettant de faire une redirection du véhicule recherché

--- src/components/part/Recherche.js
+++ src/components/part/Recherche.js
@@ -1,12 +1,14 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faMagnifyingGlass,
   faChevronRight,
 } from "@fortawesome/free-solid-svg-icons";
+import { Link } from "react-router-dom";
 
 function Recherche({ dataVehicules }) {
   const [valueSearch, setValueSearch] = useState("");
+  // const [placeHolder, setPlaceHolder] = useState(false);
   const changeValue = (event) => {
     let value = event.target.value;
     if (value === "") {
@@ -15,6 +17,15 @@ function Recherche({ dataVehicules }) {
       setValueSearch(value);
     }
   };
+  const engineSearch = (nom) => {
+    console.log(nom);
+    // dataVehicules.map((data) => (
+    //   data.nom_vehicules === nom ? (setPlaceHolder(true)) : setPlaceHolder(false)
+    // ))
+  }
+  useEffect(() => {
+    // console.log(placeHolder);
+  });
   return (
     <>
       <section className="recherche-case">
@@ -24,9 +35,10 @@ function Recherche({ dataVehicules }) {
             placeholder="Rechercher votre véhicule"
             onChange={changeValue}
             name="searchVehicule"
+            autoComplete="off"
           />
           <div className="flex-button">
-            <button type="button" className="button-recherche">
+            <button type="button" className="button-recherche" onClick={engineSearch(valueSearch)}>
               <FontAwesomeIcon
                 icon={faMagnifyingGlass}
                 className="search-icon"
@@ -39,24 +51,28 @@ function Recherche({ dataVehicules }) {
         <>
           <div className="grid_recherche">
             {dataVehicules
-              .filter((val) =>
-                val.nom_vehicules
-                  .toLowerCase()
-                  .includes(valueSearch.toLowerCase())
+              .filter((val) => 
+                  val.nom_vehicules
+                    .toLowerCase()
+                    .includes(valueSearch.toLowerCase().trim())
               )
               .map((val, index) => (
                 <>
-                  <div className="flex_liste_recherche">
-                    <div className="liste_recherche">
-                      <span key={index}>{val.nom_vehicules}</span>
-                      <span>
-                        <FontAwesomeIcon
-                          icon={faChevronRight}
-                          className="chevron_recherche"
-                        />
-                      </span>
+                  <Link to={`/vehicules/${val._id}`} style={{textDecoration: 'none'}}>                 
+                    <div className="flex_liste_recherche">
+                      <div className="liste_recherche">
+                        <span key={index} data-index={val.id_vehicules}>
+                          {val.nom_vehicules}
+                        </span>
+                        <span>
+                          <FontAwesomeIcon
+                            icon={faChevronRight}
+                            className="chevron_recherche"
+                          />
+                        </span>
+                      </div>
                     </div>
-                  </div>
+                  </Link>
                 </>
               ))}
           </div>
